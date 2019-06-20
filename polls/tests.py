4b5ef6b7f7e8feec4612d31c8f17aab92358f2b2
dtlisir <dtlisir.163.com>
2019-06-20 16:12:29 +0800

    update

--- polls/tests.py
+++ polls/tests.py
@@ -3,18 +3,54 @@
 # date: 2019/6/20
 from rest_framework.test import APITestCase
 from rest_framework.test import APIRequestFactory
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
+from django.contrib.auth import get_user_model
 from polls import apiviews
 
 class PollTest(APITestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
+        self.client = APIClient()
         self.view = apiviews.PollViewSet.as_view({'get': 'list'})
         self.uri = '/polls/'
+        self.user = self.setup_user()
+        self.token = Token.objects.create(user=self.user)
+        self.token.save()
+
+        self.client = APIClient()
+
+    @staticmethod
+    def setup_user():
+        User = get_user_model()
+        return User.objects.create_user(
+            username='test',
+            email='test@test.com',
+            password='test'
+        )
 
     def test_list(self):
-        request = self.factory.get(self.uri)
+        request = self.factory.get(self.uri,
+                                   HTTP_AUTHORIZATION='Token {}'.format(self.token.key))
         response = self.view(request)
-        self.assertEqual(response.status_code, 300,
+        self.assertEqual(response.status_code, 200,
                          'Expected Response Code 200, received {0} instead.'
                          .format(response.status_code))
 
+    def test_list2(self):
+        self.client.login(username='test', password='test')
+        response = self.client.get(self.uri)
+        self.assertEqual(response.status_code, 200,
+                         'Expected Response Code 200, received {0} instead.'
+                         .format(response.status_code))
+
+    def test_create(self):
+        self.client.login(username='test', password='test')
+        params = {
+            'question': 'question001',
+            'create_by': 1,
+        }
+        response = self.client.post(self.uri, params)
+        self.assertEqual(response.status_code, 201,
+                         'Expected Response Code 201, received {0} instead.'
+                         .format(response.status_code))
